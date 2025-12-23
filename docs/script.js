ae2b043411b56f29bbb2cc2d797acd67ff4e3315
Wakaba3 <219236104+Wakaba3@users.noreply.github.com>
2025-12-23 09:10:08 +0900

    Fixed typo

--- docs/script.js
+++ docs/script.js
@@ -2,4 +2,4 @@
 addEventListener("contextmenu", event => event.preventDefault(), { passive: false });
 
 // Prevent touch scrolling
-addEventListener("toutchmove", event => event.preventDefault(), { passive: false });
+addEventListener("touchmove", event => event.preventDefault(), { passive: false });
