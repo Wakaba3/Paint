ee761c3f9b25379ae35d8aba0659d4d20eec70d0
Wakaba3 <219236104+Wakaba3@users.noreply.github.com>
2026-01-07 23:00:56 +0900

    Paint

--- docs/paint.js
+++ docs/paint.js
@@ -281,17 +281,16 @@ class Paint {
         this.#repaint = true;
 
         // Background renderer
+        this.#loadBackground(0, 0, 0, 0);
         this.#background = this.set(0, 0, 0, 1, 0, () => this.#context.drawImage(this.#backgroundBuffer, 0, 0));
 
         // layer renderer
+        this.#loadLayers();
         this.#layers = this.set(10, 0, 0, 1, 0, this.#createImageRenderer(this.#layersBuffer));
 
         //Grid renderer
-        this.#grid = this.set(20, 0, 0, 1, 0, this.#createImageRenderer(this.#gridBuffer));
-
-        this.#loadBackground(0, 0, 0, 0);
-        this.#loadLayers();
         this.#loadGrid(255, 255, 255, 64);
+        this.#grid = this.set(20, 0, 0, 1, 0, this.#createImageRenderer(this.#gridBuffer));
     }
 
     resize(width = 0, height = 0) {
@@ -455,6 +454,7 @@ class Paint {
         const rows = (this.#view.height - 1) / height;
 
         this.#buffer.context.clearRect(0, 0, this.#buffer.width, this.#buffer.height);
+
         this.#buffer.context.strokeStyle = `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`;
         this.#buffer.context.lineWidth = 1;
         this.#buffer.context.beginPath();
@@ -473,6 +473,7 @@ class Paint {
 
         this.#buffer.context.stroke();
         this.#gridBuffer = this.#buffer.canvas.transferToImageBitmap();
+        
         this.#buffer.context.clearRect(0, 0, this.#buffer.width, this.#buffer.height);
 
         this.repaint();
