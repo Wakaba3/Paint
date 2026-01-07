a4f1abd504c394c611e8dbc9a5dbb2ef3b590771
Wakaba3 <219236104+Wakaba3@users.noreply.github.com>
2026-01-07 13:25:08 +0900

    Debug

--- docs/paint.js
+++ docs/paint.js
@@ -280,7 +280,7 @@ class Paint {
         this.set(1, 0, 0, 1, 0, this.#layers);
 
         //Grid renderer
-        this.set(2, 0, 0, 1, 30, this.#grid);
+        this.set(2, 0, 0, 1, 0, this.#grid);
     }
 
     #bind(index = 0) {
@@ -323,8 +323,8 @@ class Paint {
             renderer = (x, y, scale, angle) => {
                 this.#context.translate(-this.#view.width / 2, -this.#view.height / 2);
                 this.#context.rotate(angle * Paint.#RADIAN);
-                this.#context.translate(this.#view.width / 2, this.#view.height / 2);
                 this.#context.scale(scale, scale);
+                this.#context.translate(this.#view.width / 2, this.#view.height / 2);
                 this.#context.drawImage(image, 0, 0);
                 this.#context.resetTransform();
             };
